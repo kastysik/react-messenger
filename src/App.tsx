c31f0c454daf50473d76183fd07558cfc52fe3ab
Kazushchyk, Kiryl <kastys1@yandex.ru>
2023-06-13 14:09:31 +0200

    Fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import './App.css';
 import ChatApp from './components/Chat/ChatApp';
-import { Socket } from 'socket.io-client';
-import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import GuardedRoute from './components/GuardedRoute';
-import { useAppSelector } from './redux/store';
-import { selectUsers } from './redux/slice';
+import { io } from "socket.io-client";
+import { apiConfig } from "./config/config";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import GuardedRoute from "./components/GuardedRoute";
+import { useAppSelector } from "./redux/store";
+import { selectUsers } from "./redux/slice";
 
-// @ts-ignore
-const socket = null as Socket;
+const socket = io(apiConfig.api);
 
 function App() {
     const user = useAppSelector((state) =>
         selectUsers(state)
     );
     return (
-        <div>
-            <Routes>
-                <Route path="/" element={<Home socket={socket}/>} />
-                <Route element={<GuardedRoute auth={!!user}/>}>
-                    <Route path="/chat" element={<ChatApp socket={socket}/>} />
-                </Route>
-            </Routes>
-        </div>
+        <BrowserRouter>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Home socket={socket}/>} />
+                    <Route element={<GuardedRoute auth={!!user}/>}>
+                        <Route path="/chat" element={<ChatApp socket={socket}/>} />
+                    </Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
