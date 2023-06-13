0030e94abacd4f24635f1d0ae1ccd89d2a7a42ce
Kazushchyk, Kiryl <kastys1@yandex.ru>
2023-06-13 17:47:16 +0200

    Fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import ChatApp from './components/Chat/ChatApp';
-import { io } from "socket.io-client";
-import { apiConfig } from "./config/config";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home/Home";
-import GuardedRoute from "./components/GuardedRoute";
-import { useAppSelector } from "./redux/store";
-import { selectUsers } from "./redux/slice";
+import { io } from 'socket.io-client';
+import { apiConfig } from './config/config';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import GuardedRoute from './components/GuardedRoute';
+import { useAppSelector } from './redux/store';
+import { selectUsers } from './redux/slice';
 
 const socket = io(apiConfig.api);
 
@@ -16,7 +16,7 @@ function App() {
         selectUsers(state)
     );
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div>
                 <Routes>
                     <Route path="/" element={<Home socket={socket}/>} />
@@ -25,7 +25,7 @@ function App() {
                     </Route>
                 </Routes>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
