43d26eee831f5f14f039bbd1d926f7f6c535f98b
Kazushchyk, Kiryl <kastys1@yandex.ru>
2023-06-13 12:57:09 +0200

    Fix deployment

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import ChatApp from './components/Chat/ChatApp';
 import { io } from "socket.io-client";
-import config from "./config/config";
+import { apiConfig } from "./config/config";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import GuardedRoute from "./components/GuardedRoute";
 import { useAppSelector } from "./redux/store";
 import { selectUsers } from "./redux/slice";
 
-const socket = io(config.api);
+const socket = io(apiConfig.api);
 
 function App() {
     const user = useAppSelector((state) =>
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
-export default {
+export const apiConfig = {
     api: 'http://localhost:4008'
 }
